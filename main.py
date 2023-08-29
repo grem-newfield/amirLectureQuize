578a25f6083aa1908ce49d300846db58da2ac5db
grem-newfield <novoger123@gmail.com>
2023-08-29 13:22:52 +0300

    rev1

---
+++ main.py
@@ -0,0 +1,6 @@
+# German Novopoltsev
+
+accel = float(input("Input acceleration due to gravity value: "))
+mass = float(input("Input mass value: "))
+weight = mass * accel
+print("Calculated weight: ", weight)
